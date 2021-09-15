97dd2504cf996b230ba57eaa429eaada517fb3fe
Sahil <sahilnanda1995@gmail.com>
2021-09-15 19:54:26 +0530

    interfaces updated

--- ITransactionData.ts
+++ ITransactionData.ts
@@ -1,5 +1,11 @@
 export interface ITransactionData {
   stashId: string;
+  controllerId: string;
+  injectorId: string;
+  transactionType: string;
+  walletType: string;
+  ysFees: number;
+  ysFeesPaid: boolean;
   network: string;
   alreadyBonded: number;
   stake: number;
